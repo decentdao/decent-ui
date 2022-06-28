cf2c55e159c303a1d3c5dfc5b42baafbf5e23655
David Colon <dcolonantonio@gmail.com>
2022-06-27 22:04:41 -0400

    setup project - install tailwind - creates initial file structure - adds tsconfig - adds eslintignore - adds gitignore

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "stories": [
+    "../packages/**/*.stories.mdx",
+    "../packages/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    'storybook-addon-designs',
+    'storybook-dark-mode'
+  ],
+  "framework": "@storybook/react"
+}
