c1bee787fdb0c2d9c9b00839666c66b1bf8cd000
David Colon <dcolonantonio@gmail.com>
2022-06-30 18:51:13 -0400

    adds text styles

--- design/atoms/theme/text/index.ts
+++ design/atoms/theme/text/index.ts
@@ -1,1 +1,146 @@
-export default {}
+export const FONT_WEIGHT = {
+  BOLD: 700,
+  SEMI: 600,
+  MEDIUM: 500,
+  NORMAL: 400
+}
+
+const FONT_FAMILY = {
+  MONO: 'IBM Plex Mono',
+  SANS: 'IBM Plex Sans',
+}
+const text = {
+  'text-6xl-sans': {
+    fontFamily: FONT_FAMILY.SANS,
+    fontSize: '3em',
+    fontWeight: FONT_WEIGHT.SEMI,
+    lineHeight: '4.25em',
+    letterSpacing: '1.92px'
+  },
+  'text-5xl-sans': {
+    fontFamily: FONT_FAMILY.SANS,
+    fontSize: '2.5em',
+    fontWeight: FONT_WEIGHT.SEMI,
+    lineHeight: '4.25em',
+    letterSpacing: '1.92px'
+  },
+  'text-4xl-sans': {
+    fontFamily: FONT_FAMILY.SANS,
+    fontSize: '2em',
+    fontWeight: FONT_WEIGHT.SEMI,
+    lineHeight: '3.75em',
+    letterSpacing: '1.2px'
+  },
+  'text-4xl-mono': {
+    fontFamily: FONT_FAMILY.MONO,
+    fontSize: '2em',
+    fontWeight: FONT_WEIGHT.SEMI,
+    lineHeight: '3.75em',
+    letterSpacing: '1.2px'
+  },
+  'text-3xl-sans': {
+    fontFamily: FONT_FAMILY.SANS,
+    fontSize: '1.875em',
+    fontWeight: FONT_WEIGHT.SEMI,
+    lineHeight: '2.75em',
+    letterSpacing: '0.96px'
+  },
+  'text-3xl-mono': {
+    fontFamily: FONT_FAMILY.MONO,
+    fontSize: '1.875em',
+    fontWeight: FONT_WEIGHT.SEMI,
+    lineHeight: '2.75em',
+    letterSpacing: '0.96px'
+  },
+  'text-2xl-sans': {
+    fontFamily: FONT_FAMILY.SANS,
+    fontSize: '1.625em',
+    fontWeight: FONT_WEIGHT.SEMI,
+    lineHeight: '2.375em',
+    letterSpacing: '0.28px'
+  },
+  'text-2xl-mono': {
+    fontFamily: FONT_FAMILY.MONO,
+    fontSize: '1.625em',
+    fontWeight: FONT_WEIGHT.SEMI,
+    lineHeight: '2.375em',
+    letterSpacing: '0.28px'
+  },
+  'text-xl-sans': {
+    fontFamily: FONT_FAMILY.SANS,
+    fontSize: '1.375em',
+    fontWeight: FONT_WEIGHT.MEDIUM,
+    lineHeight: '2.125em'
+  },
+  'text-xl-mono': {
+    fontFamily: FONT_FAMILY.MONO,
+    fontSize: '1.375em',
+    fontWeight: FONT_WEIGHT.MEDIUM,
+    lineHeight: '2.125em'
+  },
+  'text-lg-sans': {
+    fontFamily: FONT_FAMILY.SANS,
+    fontSize: '1.25em',
+    fontWeight: FONT_WEIGHT.SEMI,
+    letterSpacing: '0.04em',
+    lineHeight: '1.75em'
+  },
+  'text-lg-mono': {
+    fontFamily: FONT_FAMILY.MONO,
+    fontSize: '1.25em',
+    fontWeight: FONT_WEIGHT.SEMI,
+    letterSpacing: '0.04em',
+    lineHeight: '1.75em'
+  },
+  'text-button': {
+    fontFamily: FONT_FAMILY.MONO,
+    fontSize: '1em',
+    fontWeight: FONT_WEIGHT.SEMI,
+    letterSpacing: '0.64px',
+    lineHeight: '1.5em'
+  },
+  'text-button-sm': {
+    fontFamily: FONT_FAMILY.MONO,
+    fontSize: '0.875em',
+    fontWeight: FONT_WEIGHT.SEMI,
+    letterSpacing: '0.64px',
+    lineHeight: '1.75em'
+  },
+  'text-base': {
+    fontFamily: FONT_FAMILY.SANS,
+    fontSize: '0.75em',
+    fontWeight: FONT_WEIGHT.MEDIUM,
+    letterSpacing: '0px',
+    lineHeight: '1.5em'
+  },
+  'text-sm-sans': {
+    fontFamily: FONT_FAMILY.SANS,
+    fontSize: '0.75em',
+    fontWeight: FONT_WEIGHT.MEDIUM,
+    letterSpacing: '0px',
+    lineHeight: '1.25em'
+  },
+  'text-sm-sans-regular': {
+    fontFamily: FONT_FAMILY.SANS,
+    fontSize: '0.75em',
+    fontWeight: FONT_WEIGHT.NORMAL,
+    letterSpacing: '0px',
+    lineHeight: '1.25em'
+  },
+  'text-sm-mono': {
+    fontFamily: FONT_FAMILY.MONO,
+    fontSize: '0.75em',
+    fontWeight: FONT_WEIGHT.MEDIUM,
+    letterSpacing: '0px',
+    lineHeight: '1.25em'
+  },
+  'text-xs': {
+    fontFamily: FONT_FAMILY.MONO,
+    fontSize: '0.625em',
+    fontWeight: FONT_WEIGHT.MEDIUM,
+    letterSpacing: '0px',
+    lineHeight: '1em'
+  }
+}
+
+export default text
