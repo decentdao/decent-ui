91769d3034c9bc19aed8a02c699a99c1dcbede34
David Colon <dcolonantonio@gmail.com>
2022-06-29 23:16:26 -0400

    adds theming file structure

---
+++ design/atoms/theme/index.ts
@@ -0,0 +1,17 @@
+import borders from './borders'
+import breakpoints from './breakpoints'
+import colors from './colors'
+import font from './font'
+import radius from './radius'
+import spacing from './spacing'
+import text from './text'
+
+export default {
+  borders,
+  breakpoints,
+  spacing,
+  colors,
+  radius,
+  font,
+  text,
+};
