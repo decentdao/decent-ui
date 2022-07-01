d3f3b12955cf1513f279f4784a1e69fae6c27ff0
David Colon <dcolonantonio@gmail.com>
2022-06-30 20:34:57 -0400

    removed unused theme files

--- design/atoms/theme/index.ts
+++ design/atoms/theme/index.ts
@@ -1,19 +1,11 @@
 import borders from './borders'
 import breakpoints from './breakpoints'
 import colors from './colors'
-import font from './font'
-import radius from './radius'
-import spacing from './spacing'
 import text from './text'
-import typography from './typography'
 
 export default {
   borders,
   breakpoints,
-  spacing,
   colors,
-  radius,
-  font,
-  text,
-  typography
+  text
 };
