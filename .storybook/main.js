33e19097574d940d824f1f02bde4ec13622d2ce1
Sean Reid <sean@torchcodelab.com>
2022-07-06 18:00:19 -0400

    WIP - Dependency hell getting all the plugins to play nicely

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,39 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
-  "stories": [
-    "../design/**/*.stories.mdx",
-    "../design/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-docs",
-    "@chakra-ui/storybook-addon",
-    'storybook-addon-designs',
-    'storybook-dark-mode'
-  ],
-  "framework": "@storybook/react",
-  staticDirs: ['../design/atoms/assets']
-}
+    stories: ['../design/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/addon-docs',
+        '@chakra-ui/storybook-addon',
+        'storybook-addon-designs',
+        'storybook-dark-mode',
+        {
+            name: '@storybook/addon-postcss',
+            options: {
+                postcssLoaderOptions: {
+                    implementation: require('postcss'),
+                },
+            },
+        },
+    ],
+    webpackFinal: async (config, { configType }) => {
+        config.plugins.push(
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, '../src/fonts'),
+                        to: 'static/fonts',
+                    },
+                ],
+            })
+        );
+
+        return config;
+    },
+    framework: '@storybook/react',
+    staticDirs: ['../design/atoms/assets'],
+};
