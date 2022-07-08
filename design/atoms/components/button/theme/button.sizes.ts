dfd5abf9e34564879882733011ff9e7d0ccc71cc
David Colon <dcolonantonio@gmail.com>
2022-07-08 06:24:27 -0400

    updates button sizes with font

--- design/atoms/components/button/theme/button.sizes.ts
+++ design/atoms/components/button/theme/button.sizes.ts
@@ -1,10 +1,12 @@
 export default {
   lg: {
+    apply: 'textStyles.text-button',
     height: '3.75rem',
     width: 'auto',
     padding: '1rem 3rem'
   },
   base: {
+    apply: 'textStyles.text-button-sm',
     height: '2.5rem',
     width: 'auto',
     padding: '0.375rem 1.5rem',
