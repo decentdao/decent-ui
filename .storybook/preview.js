65008e628f0404473b1acaceaac0b491bb6713b1
Sean Reid <sean@torchcodelab.com>
2022-07-13 15:46:43 -0400

    Added Documentation for Design Language items
    * Updated sorting for Storybook sidebar
    * Added documention from Notion

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,14 +3,21 @@ import {addDecorator} from '@storybook/react';
 import theme from '../design/tokens/theme';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  chakra: {
-    theme,
-  },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    chakra: {
+        theme,
     },
-  },
-}
+    controls: {
+        hideNoControlsWarning: true,
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+    options: {
+        storySort: {
+            method: 'alphabetical',
+            order: ['Design Language', 'Design Tokens'],
+        },
+    },
+};
