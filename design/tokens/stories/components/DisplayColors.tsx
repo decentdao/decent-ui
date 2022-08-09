e6118db7574644a624b8b621c86ab94a1c27b1ce
Sean R Reid <sean@torchcodelab.com>
2022-08-09 15:16:09 -0400

    Working through nested JSON in tokens

--- design/tokens/stories/components/DisplayColors.tsx
+++ design/tokens/stories/components/DisplayColors.tsx
@@ -1,5 +1,5 @@
-import { Box, Text } from "@chakra-ui/react"
-import { StoryLayout } from "../../../shared/StoryLayout"
+import { Box, Text } from '@chakra-ui/react';
+import { StoryLayout } from '../../../shared/StoryLayout';
 // import colors from "../../foundations/colors"
 import tokenData from '../../foundations/colors/design-tokens-colors.json';
 
@@ -7,29 +7,69 @@ const { tokens } = tokenData;
 const { color } = tokens;
 
 export function DisplayColors() {
-  return (
-    <StoryLayout title="Colors">
-        {Object.entries(color).map(([colorCategory, values], index) => {
-          return (
-            <Box key={colorCategory + index}>
-                {Object.entries(values as Object).map(([colorTitle, value]) => {
-                  const colorName = `${colorCategory}-${colorTitle}`
-                  const colorKey = `${colorCategory}.${colorTitle}`
-                  return (
-                    <Box key={colorName} display="flex" justifyContent="space-between" flexGrow={1} height="75px" marginY="12">
-                      <Box width="fit-content" display="flex" flexDirection="column" justifyContent="center">
-                        <Text fontSize="lg" marginBottom="4" textStyle="text-sm-mono">
-                          {colorName}
-                        </Text>
-                        <Text textStyle="text-sm-sans">{value}</Text>
-                      </Box>
-                      <Box marginLeft="24" flexGrow={1} bg={colorKey} maxWidth="60%" borderRadius="lg" />
+    return (
+        <StoryLayout title='Colors'>
+            {Object.entries(color).map(([colorCategory, values], index) => {
+                return (
+                    <Box key={colorCategory + index}>
+                        <ul>
+                            <li>
+                                {colorCategory}
+                                <ul style={{ marginLeft: '1rem' }}>
+                                    {Object.entries(values as Object).map(
+                                        ([colorTitle, value]) => {
+                                            console.log(
+                                                'FOOP: TYPE OF VALUE: ',
+                                                typeof value
+                                            );
+                                            return (
+                                                <li>
+                                                    {colorTitle}
+                                                    <ul
+                                                        style={{
+                                                            marginLeft: '1rem',
+                                                        }}
+                                                    >
+                                                        {Object.entries(
+                                                            value as Object
+                                                        ).map(
+                                                            ([value, type]) => {
+                                                                return (
+                                                                    value !==
+                                                                        'value' &&
+                                                                    value !==
+                                                                        'type' && (
+                                                                        <li>
+                                                                            {
+                                                                                value
+                                                                            }
+                                                                        </li>
+                                                                    )
+                                                                );
+                                                            }
+                                                        )}
+                                                    </ul>
+                                                </li>
+                                            );
+                                            //   return (
+                                            //     <Box key={colorName} display="flex" justifyContent="space-between" flexGrow={1} height="75px" marginY="12">
+                                            //       <Box width="fit-content" display="flex" flexDirection="column" justifyContent="center">
+                                            //         <Text fontSize="lg" marginBottom="4" textStyle="text-sm-mono">
+                                            //           {colorName}
+                                            //         </Text>
+                                            //         <Text textStyle="text-sm-sans">{value}</Text>
+                                            //       </Box>
+                                            //       <Box marginLeft="24" flexGrow={1} bg={colorKey} maxWidth="60%" borderRadius="lg" />
+                                            //     </Box>
+                                            //   )
+                                        }
+                                    )}
+                                </ul>
+                            </li>
+                        </ul>
                     </Box>
-                  )
-                })}
-            </Box>
-          )
-        })}
-    </StoryLayout>
-  )
+                );
+            })}
+        </StoryLayout>
+    );
 }
