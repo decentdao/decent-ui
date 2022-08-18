5961f2257f66785ddb41297e8579ad10f9ac0eb9
Sean Reid <sean@torchcodelab.com>
2022-08-18 16:00:14 -0400

    Refactored parsing of the tokens * Need to remove White/Black from "Neutral" section, causes errors * Need to look at Gradient and Utilities exports, we might need to adjust those outputs

--- design/tokens/stories/components/DisplayColors.tsx
+++ design/tokens/stories/components/DisplayColors.tsx
@@ -1,72 +1,54 @@
 import { Box, Text } from '@chakra-ui/react';
 import { StoryLayout } from '../../../shared/StoryLayout';
-// import colors from "../../foundations/colors"
-import tokenData from '../../foundations/colors/design-tokens-colors.json';
-
-const { tokens } = tokenData;
-const { color } = tokens;
+import colors from '../../foundations/colors';
 
 export function DisplayColors() {
     return (
         <StoryLayout title='Colors'>
-            {Object.entries(color).map(([colorCategory, values], index) => {
+            {Object.entries(colors).map(([colorCategory, values], index) => {
+                console.log('FOOP COLORS ARE: ', colors);
                 return (
                     <Box key={colorCategory + index}>
-                        <ul>
-                            <li>
-                                {colorCategory}
-                                <ul style={{ marginLeft: '1rem' }}>
-                                    {Object.entries(values as Object).map(
-                                        ([colorTitle, value]) => {
-                                            console.log(
-                                                'FOOP: TYPE OF VALUE: ',
-                                                typeof value
-                                            );
-                                            return (
-                                                <li>
-                                                    {colorTitle}
-                                                    <ul
-                                                        style={{
-                                                            marginLeft: '1rem',
-                                                        }}
-                                                    >
-                                                        {Object.entries(
-                                                            value as Object
-                                                        ).map(
-                                                            ([value, type]) => {
-                                                                return (
-                                                                    value !==
-                                                                        'value' &&
-                                                                    value !==
-                                                                        'type' && (
-                                                                        <li>
-                                                                            {
-                                                                                value
-                                                                            }
-                                                                        </li>
-                                                                    )
-                                                                );
-                                                            }
-                                                        )}
-                                                    </ul>
-                                                </li>
-                                            );
-                                            //   return (
-                                            //     <Box key={colorName} display="flex" justifyContent="space-between" flexGrow={1} height="75px" marginY="12">
-                                            //       <Box width="fit-content" display="flex" flexDirection="column" justifyContent="center">
-                                            //         <Text fontSize="lg" marginBottom="4" textStyle="text-sm-mono">
-                                            //           {colorName}
-                                            //         </Text>
-                                            //         <Text textStyle="text-sm-sans">{value}</Text>
-                                            //       </Box>
-                                            //       <Box marginLeft="24" flexGrow={1} bg={colorKey} maxWidth="60%" borderRadius="lg" />
-                                            //     </Box>
-                                            //   )
-                                        }
-                                    )}
-                                </ul>
-                            </li>
-                        </ul>
+                        <h2>{colorCategory}</h2>
+                        {Object.entries(values as Object).map(
+                            ([colorTitle, hexValue]) => {
+                                return (
+                                    <Box
+                                        key={colorTitle}
+                                        display='flex'
+                                        justifyContent='space-between'
+                                        flexGrow={1}
+                                        height='75px'
+                                        marginY='12'
+                                    >
+                                        <Box
+                                            width='fit-content'
+                                            display='flex'
+                                            flexDirection='column'
+                                            justifyContent='center'
+                                        >
+                                            <Text
+                                                fontSize='lg'
+                                                marginBottom='4'
+                                                textStyle='text-sm-mono'
+                                            >
+                                                {colorTitle}
+                                            </Text>
+                                            <Text textStyle='text-sm-sans'>
+                                                {hexValue.value}
+                                            </Text>
+                                        </Box>
+                                        <Box
+                                            marginLeft='24'
+                                            flexGrow={1}
+                                            bg={hexValue.value}
+                                            maxWidth='60%'
+                                            borderRadius='lg'
+                                        />
+                                    </Box>
+                                );
+                            }
+                        )}
                     </Box>
                 );
             })}
