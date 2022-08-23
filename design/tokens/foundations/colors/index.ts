51e120c1a217143c75f900e48c42ca0ed80d738b
Sean Reid <sean@torchcodelab.com>
2022-08-23 15:09:43 -0400

    Removed comments, replaced token file with updates from Design

--- design/tokens/foundations/colors/index.ts
+++ design/tokens/foundations/colors/index.ts
@@ -1,14 +1,12 @@
-// import tokenData from './design-tokens-colors.json';
-import tokenData from '../../design-tokens-export-2.json'
+import tokenData from '../../color-tokens.json'
 
-// const { tokens } = tokenData;
-const { color } = tokenData;
-const { primary, secondary, neutral, utility } = color;
+const { Colors } = tokenData;
+const { Primary, Secondary, Neutral, Utility } = Colors;
 
-const primaries = { ...primary }
-const secondaries = { ...secondary }
-const neutrals = { ...neutral }
-const utilities = { ...utility }
+const primaries = { ...Primary }
+const secondaries = { ...Secondary }
+const neutrals = { ...Neutral }
+const utilities = { ...Utility }
 // const gradients = { ...gradient }
 
 const colors = {
