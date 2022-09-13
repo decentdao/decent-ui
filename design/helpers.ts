c7986104a5d45460764b9c30eabd67753358021c
David Colon <dcolonantonio@gmail.com>
2022-09-13 18:00:14 -0400

    covers case where token has base name and selector

--- design/helpers.ts
+++ design/helpers.ts
@@ -6,26 +6,33 @@ export function parseColors(colorFigmaTokens: any) {
     const colorObj = Object.entries(tokens!).reduce((prev, cur) => {
 
       // spilts token color and number value gold-500
-      const [, potentialNumber] = cur[0].split("-")
+      const [colorName, potentialNumber] = cur[0].split("-")
 
       // spilt fails its assumed that this is a base color, ie white: {value: #FFF}
-      if(!potentialNumber) {
-        return {...prev, base: cur[1].value}
+      if (!potentialNumber) {
+        // this spilt checks for selectors (active, disabled, hover, etc)
+        const [baseName, selector] = cur[0].split(':')
+
+        // if selector exists create key using number and selector (white-active)
+        if (selector) {
+          return { ...prev, [`${baseName.toLowerCase().trim()}-${selector.trim()}`]: cur[1].value }
+        }
+        return { ...prev, [colorName.toLowerCase()]: cur[1].value }
       }
 
       // this spilt checks for selectors (active, disabled, hover, etc)
       const [colorNumber, selector] = potentialNumber.split(':')
 
       // if selector exists create key using number and selector (500-active)
-      if(selector) {
-        return {...prev, [`${colorNumber}-${selector.trim()}`]: cur[1].value}
+      if (selector) {
+        return { ...prev, [`${colorNumber}-${selector.trim()}`]: cur[1].value }
       }
 
       // returns previous object combined with new color key and value
-      return {...prev, [colorNumber]: cur[1].value}
-    }, {}) 
-    return {[colorCatogoryNames[index].toLowerCase()]: colorObj}
+      return { ...prev, [colorNumber]: cur[1].value }
+    }, {})
+    return { [colorCatogoryNames[index].toLowerCase()]: colorObj }
   }).reduce((prev, cur) => {
-    return {...prev, ...cur}
+    return { ...prev, ...cur }
   }, {})
 }
