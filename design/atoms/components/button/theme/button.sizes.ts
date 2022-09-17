5ec1cc04785f7898c0f17bd76309ed278cbc79d4
David Colon <dcolonantonio@gmail.com>
2022-09-17 12:03:19 -0400

    account for larger labels

--- design/atoms/components/button/theme/button.sizes.ts
+++ design/atoms/components/button/theme/button.sizes.ts
@@ -2,18 +2,18 @@ export default {
   lg: {
     apply: 'textStyles.text-md-mono-semibold',
     height: '3.5rem',
-    width: '9.625rem',
+    minWidth: '9.625rem',
     padding: '1rem 3rem'
   },
   base: {
     apply: 'textStyles.text-sm-mono-semibold',
     height: '2.25rem',
-    width: '6.625rem',
+    minWidth: '6.625rem',
     padding: '0.375rem 1.5rem',
   },
   sm: {
     apply: 'textStyles.text-xs-mono-semibold',
-    width: '4.75rem',
+    minWidth: '4.75rem',
     height: '1.875rem',
     padding: '0.375rem 1rem'
   }
