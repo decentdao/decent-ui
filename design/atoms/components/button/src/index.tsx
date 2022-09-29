e776d2318dfd662074d60e0a69daff2dcb827e06
David Colon <dcolonantonio@gmail.com>
2022-09-29 00:16:48 -0400

    updates Button type and adds isDisabled control

--- design/atoms/components/button/src/index.tsx
+++ design/atoms/components/button/src/index.tsx
@@ -1,20 +1,30 @@
-
-
-import { Button as ChakraButton, forwardRef, useStyleConfig } from '@chakra-ui/react'
-import { ReactElement } from 'react';
+import {
+  As,
+  Button as ChakraButton,
+  ButtonProps,
+  ComponentWithAs,
+  forwardRef,
+  useStyleConfig,
+} from "@chakra-ui/react"
+import { ReactElement } from "react"
 
 interface IButton {
-  variant?: 'primary' | 'secondary' | 'tertiary';
-  size?: 'base' | 'lg' | 'sm';
-  label?: string;
-  disabled?: boolean;
+  variant?: "primary" | "secondary" | "tertiary"
+  size?: "base" | "lg" | "sm"
+  label?: string
   leftIcon?: ReactElement
   rightIcon?: ReactElement
 }
 
-const Button = forwardRef(({variant, size, label, ...rest}: IButton, ref) => {
-  const styles = useStyleConfig('Button', { size, variant });
-  return <ChakraButton  sx={styles} ref={ref} {...rest}>{label}</ChakraButton>
-})
+export const Button = forwardRef<IButton, ComponentWithAs<"button", ButtonProps>>(
+  ({ variant, size, label, ...rest }, ref) => {
+    const styles = useStyleConfig("Button", { size, variant })
+    return (
+      <ChakraButton sx={styles} ref={ref} {...rest}>
+        {label}
+      </ChakraButton>
+    )
+  }
+)
 
 export default Button
