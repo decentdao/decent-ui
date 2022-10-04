0d9e4f2322637807071b1ddb03a2b2b6b8c11274
David Colon <dcolonantonio@gmail.com>
2022-10-04 17:45:55 -0400

    temporary update to design tokens

--- design/tokens/foundations/colors/index.ts
+++ design/tokens/foundations/colors/index.ts
@@ -2,18 +2,20 @@ import { parseColors } from '../../../helpers';
 import tokenData from '../../design-tokens-export.json'
 
 const { color } = tokenData;
-const { primary, secondary, neutral, utility } = color;
+const { primary, secondary, neutral, utility, ui } = color;
 
 const primaries = parseColors({...primary})
 const secondaries = parseColors({ ...secondary })
 const neutrals = parseColors({ ...neutral })
 const utilities = parseColors({ ...utility })
+const uiColors = parseColors({ ...ui })
 
 const colors = {
     ...primaries,
     ...secondaries,
     ...neutrals,
-    ...utilities,
+    ...uiColors,
+    ...utilities
 }
 
 export type Colors = typeof colors;
