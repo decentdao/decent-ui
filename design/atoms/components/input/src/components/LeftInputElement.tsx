7632018c4fae2f7f7247d5dafabd2c1949e0d9bb
David Colon <dcolonantonio@gmail.com>
2022-10-05 11:54:54 -0400

    remove logs

--- design/atoms/components/input/src/components/LeftInputElement.tsx
+++ design/atoms/components/input/src/components/LeftInputElement.tsx
@@ -10,9 +10,7 @@ const LeftInputElement = ({leftElement, isDisabled, size}: ILeftInputElement) =>
   if(!leftElement) {
     return null
   }
-  console.log("🚀 ~ file: LeftInputElement.tsx ~ line 10 ~ isDisabled", isDisabled)
   const color = isDisabled ? 'grayscale.800' : 'gold.500'
-  console.log("🚀 ~ file: LeftInputElement.tsx ~ line 14 ~ color", color)
   return (
     <Box pos="absolute" zIndex={1} top="7" left="12" lineHeight="24px" color={color}>
       {leftElement}
