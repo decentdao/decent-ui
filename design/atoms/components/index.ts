97516b5c0eb9dc48369941512ff32c06193078f6
David Colon <dcolonantonio@gmail.com>
2022-10-10 11:20:22 -0400

    update component export for package to include Label

--- design/atoms/components/index.ts
+++ design/atoms/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './button/src'
 export { default as Input } from './input/src'
+export { default as LabelWrapper } from './Label/src'
 export * as Icons from './icons/src'
