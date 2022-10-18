5ea0ee7d5473a1ae7252c4f2c1fec85158462af2
David Colon <dcolonantonio@gmail.com>
2022-10-18 14:53:34 -0400

    cleans up icon story

--- design/atoms/components/icons/stories/components/DisplayIconography.tsx
+++ design/atoms/components/icons/stories/components/DisplayIconography.tsx
@@ -1,62 +1,13 @@
+import { ComponentWithAs, IconProps } from "@chakra-ui/react"
 import { StoryLayout } from "../../../../../shared/StoryLayout"
-import { AddPlus } from "../../src"
-import { Alert } from "../../src"
-import { ArrowAngleUp } from "../../src"
-import { ArrowDown } from "../../src"
-import { ArrowLeft } from "../../src"
-import { ArrowRight } from "../../src"
-import { ArrowUp } from "../../src"
-import { BellAlert } from "../../src"
-import { Buttons } from "../../src"
-import { Chat } from "../../src"
-import { Check } from "../../src"
-import { Clock } from "../../src"
-import { Contact } from "../../src"
-import { Copy } from "../../src"
-import { DecentD } from "../../src"
-import { Disconnect } from "../../src"
-import { Discord } from "../../src"
-import { Documents } from "../../src"
-import { Home } from "../../src"
-import { Info } from "../../src"
-import { Members } from "../../src"
-import { Notifications } from "../../src"
-import { Proposals } from "../../src"
-import { StarGoldSolid } from "../../src"
-import { StarOutline } from "../../src"
-import { SupportQuestion } from "../../src"
-import { Treasury } from "../../src"
+import * as icons from '../../src'
 
 export function DisplayIconography() {
   return (
     <StoryLayout title="Icon">
-      <AddPlus />
-      <Alert />
-      <ArrowAngleUp />
-      <ArrowDown />
-      <ArrowLeft />
-      <ArrowRight />
-      <ArrowUp />
-      <BellAlert />
-      <Buttons />
-      <Chat />
-      <Check />
-      <Clock />
-      <Contact />
-      <Copy />
-      <DecentD />
-      <Disconnect />
-      <Discord />
-      <Documents />
-      <Home />
-      <Info />
-      <Members />
-      <Notifications />
-      <Proposals />
-      <StarGoldSolid />
-      <StarOutline />
-      <SupportQuestion />
-      <Treasury />
+      {Object.values(icons).map((Icon: ComponentWithAs<"svg", IconProps>) => (
+        <Icon boxSize="24px" m="1rem" />
+      ))}
     </StoryLayout>
   )
 }
