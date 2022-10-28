fe07dd57261e5f73025fc7f7ede063ed74eb1f25
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-10-28 15:46:11 -0400

    updates button font to match figma

--- design/atoms/components/button/theme/button.sizes.ts
+++ design/atoms/components/button/theme/button.sizes.ts
@@ -1,18 +1,18 @@
 export default {
   lg: {
-    apply: 'textStyles.text-md-mono-semibold',
+    apply: 'textStyles.text-base-mono-semibold',
     height: '3.5rem',
     minWidth: '9.625rem',
     padding: '1rem 3rem'
   },
   base: {
-    apply: 'textStyles.text-sm-mono-semibold',
+    apply: 'textStyles.text-md-mono-semibold',
     height: '2.25rem',
     minWidth: '6.625rem',
     padding: '0.375rem 1.5rem',
   },
   sm: {
-    apply: 'textStyles.text-xs-mono-semibold',
+    apply: 'textStyles.text-sm-mono-semibold',
     minWidth: '4.75rem',
     height: '1.875rem',
     padding: '0.375rem 1rem'
