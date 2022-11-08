ff6bbc7da0fac0f520be56b281a7384a0fda2a5e
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-11-08 18:35:39 -0500

    fixes console errors

--- design/tokens/styles/input.ts
+++ design/tokens/styles/input.ts
@@ -1,10 +1,10 @@
 export default {
   'input:not(.step-counter)::-webkit-outer-spin-button, input:not(.step-counter)::-webkit-inner-spin-button': {
-    '-webkit-appearance': 'none',
+    'WebkitAppearance': 'none',
     margin: 0
   },
 
   'input: not(.step - counter)[type = number]': {
-    '-moz-appearance': 'textfield'
+    'MozAppearance': 'textfield'
   },
 }
