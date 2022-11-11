e13bf4071568ebf5b89e2602fb222c7bf9cc8973
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-11-11 13:47:40 -0500

    updates icon story

--- design/atoms/components/icons/stories/components/DisplayIconography.tsx
+++ design/atoms/components/icons/stories/components/DisplayIconography.tsx
@@ -1,20 +1,17 @@
 import { ComponentWithAs, Flex, IconProps, Text } from "@chakra-ui/react"
-import { StoryLayout } from "../../../../../shared/StoryLayout"
 import * as icons from '../../src'
 
 export function DisplayIconography() {
   return (
-    <StoryLayout title="Icon">
-      <Flex flexWrap="wrap" justifyContent="space-between" gap="1rem">
-        {Object.values(icons).map((Icon: ComponentWithAs<"svg", IconProps>, i) => {
-          return (
-            <Flex key={i} flexDirection="column" w="13.75rem" h="10rem" justifyContent="center" alignItems="center" p="1rem" border="1px solid" borderColor="chocolate.500">
-              <Text textStyle="text-base-mono-bold" color="grayscale.100">{Icon.displayName}</Text>
-              <Icon boxSize="1.5rem" m="1rem" />
-            </Flex>
-          )
-        })}
-      </Flex>
-    </StoryLayout>
+    <Flex flexWrap="wrap" justifyContent="space-between" gap="1rem">
+      {Object.values(icons).map((Icon: ComponentWithAs<"svg", IconProps>, i) => {
+        return (
+          <Flex key={i} flexDirection="column" w="13.75rem" h="10rem" justifyContent="center" alignItems="center" p="1rem" border="1px solid" borderColor="chocolate.500">
+            <Text textStyle="text-base-mono-bold" color="grayscale.100">{Icon.displayName}</Text>
+            <Icon boxSize="3rem" m="1rem" />
+          </Flex>
+        )
+      })}
+    </Flex>
   )
 }
