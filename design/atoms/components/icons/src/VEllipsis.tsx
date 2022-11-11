b738d80495f60dbac164b2ec3dc943f0efe3d215
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-11-11 13:42:41 -0500

    adds new icons - removes image stories; converts to icons

---
+++ design/atoms/components/icons/src/VEllipsis.tsx
@@ -0,0 +1,9 @@
+import { createIcon } from "@chakra-ui/react"
+
+export const VEllipsis = createIcon({
+  displayName: "VEllipsis",
+  viewBox: "0 0 24 24",
+  path: (
+    <path d="M12 3c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2Zm0 14c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2Zm0-7c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2Z" fill="currentColor" />
+  ),
+})
