d57b68381d996476a4b181d9e41b42866e0fc952
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-11-11 00:36:07 -0500

    clean up icon story

--- design/atoms/components/icons/stories/components/DisplayIconography.tsx
+++ design/atoms/components/icons/stories/components/DisplayIconography.tsx
@@ -1,13 +1,20 @@
-import { ComponentWithAs, IconProps } from "@chakra-ui/react"
+import { ComponentWithAs, Flex, IconProps, Text } from "@chakra-ui/react"
 import { StoryLayout } from "../../../../../shared/StoryLayout"
 import * as icons from '../../src'
 
 export function DisplayIconography() {
   return (
     <StoryLayout title="Icon">
-      {Object.values(icons).map((Icon: ComponentWithAs<"svg", IconProps>) => (
-        <Icon boxSize="24px" m="1rem" />
-      ))}
+      <Flex flexWrap="wrap" justifyContent="space-between" gap="1rem">
+        {Object.values(icons).map((Icon: ComponentWithAs<"svg", IconProps>, i) => {
+          return (
+            <Flex key={i} flexDirection="column" w="13.75rem" h="10rem" justifyContent="center" alignItems="center" p="1rem" border="1px solid" borderColor="chocolate.500">
+              <Text textStyle="text-base-mono-bold" color="grayscale.100">{Icon.displayName}</Text>
+              <Icon boxSize="1.5rem" m="1rem" />
+            </Flex>
+          )
+        })}
+      </Flex>
     </StoryLayout>
   )
 }
