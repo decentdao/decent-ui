52ff9ef21942f832ef40c79f764865daad47d41c
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-11-25 12:38:40 -0500

    adds Input group custom component

--- design/atoms/components/index.ts
+++ design/atoms/components/index.ts
@@ -1,3 +1,4 @@
 export { default as NumberInput } from './numberInput/src'
+export { default as InputGroup } from './inputGroup/src'
 export { default as LabelWrapper } from './label/src'
 export * from './icons/src'
