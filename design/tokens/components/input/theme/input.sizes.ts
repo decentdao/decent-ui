178614bba2c803ec66e368803669a3f21887d028
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-11-26 13:46:06 -0500

    adds left element example story

--- design/tokens/components/input/theme/input.sizes.ts
+++ design/tokens/components/input/theme/input.sizes.ts
@@ -4,27 +4,47 @@ import { createMultiStyleConfigHelpers, defineStyle } from '@chakra-ui/react'
 const { definePartsStyle } =
   createMultiStyleConfigHelpers(inputAnatomy.keys)
 
-const base = defineStyle({
+const baseStyle = {
   apply: 'text-base-mono-semibold',
-  px: '1rem',
   height: '2.5rem',
   _placeholder: {
     textStyle: 'text-base-mono-medium'
   },
+}
+const base = defineStyle({
+  ...baseStyle,
+  px: '1rem'
 })
 
-const xl = defineStyle({
+const baseWithAddon = defineStyle({
+  ...baseStyle,
+  px: '2rem'
+})
+
+const xlStyle = {
   apply: 'text-xl-mono-semibold',
   h: '4.375rem',
   px: '1rem',
   _placeholder: {
     textStyle: 'text-xl-mono-medium'
   },
+}
+
+const xl = defineStyle({
+  ...xlStyle,
+  px: '1rem',
+})
+
+const xlWithAddon = defineStyle({
+  ...xlStyle,
+  px: '2rem',
 })
 
 const sizes = {
   base: definePartsStyle({ field: base, addon: base }),
+  baseWithAddon: definePartsStyle({ field: baseWithAddon, addon: baseWithAddon }),
   xl: definePartsStyle({ field: xl, addon: xl }),
+  xlWithAddon: definePartsStyle({ field: xlWithAddon, addon: xlWithAddon }),
 }
 
 export default sizes
