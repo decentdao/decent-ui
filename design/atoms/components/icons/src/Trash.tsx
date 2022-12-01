2b1a061ac3bd3d48183461c9bcbd1eb376a23353
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-11-30 20:50:58 -0500

    add new icons - add queued icon - add shield icon - add trash icon

---
+++ design/atoms/components/icons/src/Trash.tsx
@@ -0,0 +1,9 @@
+import { createIcon } from "@chakra-ui/react"
+
+export const Trash = createIcon({
+  displayName: "Trash",
+  viewBox: "0 0 24 24",
+  path: (
+    <path d="M9 3v1H4v2h1v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1V4h-5V3H9ZM7 6h10v13H7V6Zm2 2v9h2V8H9Zm4 0v9h2V8h-2Z" fill="currentColor" />
+  ),
+})
