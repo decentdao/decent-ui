01a65d6e22c94edf765bdd9f9c5ee1e359145c21
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-12-01 10:36:02 -0500

    subLabel can be passed ReactNode

--- design/molecules/components/labelWrapper/src/types/index.ts
+++ design/molecules/components/labelWrapper/src/types/index.ts
@@ -1,6 +1,8 @@
+import { ReactNode } from "react";
+
 export declare interface ILabelWrapper {
   label?: string;
-  subLabel?: string;
+  subLabel?: ReactNode;
   isDisabled?: boolean;
   errorMessage?: string;
   tooltipContent?: JSX.Element;
