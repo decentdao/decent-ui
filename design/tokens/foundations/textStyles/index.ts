d0d0e22fc1db218d8479fe5c9b4f6d223520b8fe
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-11-30 19:17:47 -0500

    creates typography parser method

--- design/tokens/foundations/textStyles/index.ts
+++ design/tokens/foundations/textStyles/index.ts
@@ -1,43 +1,13 @@
+import { parseTypography } from '../../../helpers';
 import tokenData from '../../design-tokens-export.json'
 
-const { font } = tokenData
+const { font: { sans, mono } } = tokenData
 
-const SANS: { [key: string]: {} } = {};
-const MONO: { [key: string]: {} } = {};
+export const sansTypography: { [key: string]: { [key: string]: {} } } = parseTypography(sans)
+export const monoTypography: { [key: string]: { [key: string]: {} } } = parseTypography(mono)
 
-// @TODO
-// This can probably be refactored into a single method to cover all font types?
-// - SR
-Object.entries(font.sans).flatMap((style) => {
-    Object.entries(style[1]).map((weight) => {
-        const fontStyle = `${style[0]}-${weight[0]}`;
-        const { fontSize, fontFamily, fontWeight, letterSpacing } = weight[1].value;
-        SANS[fontStyle] = {
-            fontSize,
-            lineHeight: "1.5",
-            letterSpacing,
-            fontFamily,
-            fontWeight
-        }
-    });
-    // @TODO - It might be handy to sort() these?? - SR
-})
-
-Object.entries(font.mono).flatMap((style) => {
-    Object.entries(style[1]).map((weight) => {
-        const fontStyle = `${style[0]}-${weight[0]}`;
-        const { fontSize, fontFamily, fontWeight, letterSpacing } = weight[1].value;
-        MONO[fontStyle] = {
-            fontSize,
-            lineHeight: "1.5",
-            letterSpacing,
-            fontFamily,
-            fontWeight
-        }
-    });
-})
 
 export default {
-    ...SANS,
-    ...MONO
+    ...sansTypography,
+    ...monoTypography
 }
