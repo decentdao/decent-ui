b8b6921e9ce9a90df976d9f262a45356c493b61a
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-12-15 19:45:20 -0500

    updates FractalBrand logo

--- design/atoms/components/icons/src/FractalBrand.tsx
+++ design/atoms/components/icons/src/FractalBrand.tsx
@@ -2,20 +2,25 @@ import { createIcon } from "@chakra-ui/react"
 
 export const FractalBrand = createIcon({
   displayName: "FractalBrand",
-  viewBox: "0 0 68 68",
+  viewBox: "0 0 74 74",
   path: (
     <svg>
-      <rect width="68" height="68" rx="8" fill="url(#lgFractalBrand-a)" />
-      <path d="M26 14h18v6.384H32.504v10.862H41.5v6.435h-8.996V54H26V14Z" fill="url(#lgFractalBrand-b)" />
+      <g filter="url(#filter0_d_204_1439)">
+        <rect x="16" y="16" width="36" height="36" rx="5" fill="#FDCB57" />
+      </g>
+      <path d="M42.8453 23.8215V26.9002H28.7046V32.8062H40.2913V35.7591H28.7046V44.1783H25.1538V35.7591H28.7046V32.8062H25.1538V23.8215H42.8453Z" fill="#36342F" />
       <defs>
-        <linearGradient id="lgFractalBrand-a" x1="34" y1="0" x2="34" y2="68" gradientUnits="userSpaceOnUse">
-          <stop stopColor="#272520" />
-          <stop offset="1" stopColor="#1B1A18" />
-        </linearGradient>
-        <linearGradient id="lgFractalBrand-b" x1="35" y1="14" x2="35" y2="54" gradientUnits="userSpaceOnUse">
-          <stop stopColor="#FABD2E" />
-          <stop offset="1" stopColor="#FFD36E" />
-        </linearGradient>
+        <filter id="filter0_d_204_1439" x="-6" y="-6" width="80" height="80" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+          <feMorphology radius="2" operator="dilate" in="SourceAlpha" result="effect1_dropShadow_204_1439" />
+          <feOffset />
+          <feGaussianBlur stdDeviation="10" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0.686275 0 0 0 0 0.517647 0 0 0 0 0.12549 0 0 0 0.5 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_204_1439" />
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_204_1439" result="shape" />
+        </filter>
       </defs>
     </svg>
 
