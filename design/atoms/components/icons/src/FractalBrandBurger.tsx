87369121a11ed257cceec69c1cdc836fede12751
David Colon <38386583+Da-Colon@users.noreply.github.com>
2023-02-21 20:37:18 -0500

    add fractal brand burger menu

---
+++ design/atoms/components/icons/src/FractalBrandBurger.tsx
@@ -0,0 +1,19 @@
+import { createIcon } from "@chakra-ui/react"
+
+export const FractalBrandBurger = createIcon({
+  displayName: "FractalBrandBurger",
+  viewBox: "0 0 74 74",
+  path: (
+    <svg>
+      <circle cx="18.8734" cy="5.12739" r="2.12727" fill="#FABD2E" />
+      <circle cx="12.0003" cy="12.0003" r="2.12727" fill="#FABD2E" />
+      <circle cx="12.0003" cy="5.12739" r="2.12727" fill="#FABD2E" />
+      <circle cx="5.12727" cy="18.8724" r="2.12727" fill="#FABD2E" />
+      <circle cx="5.12727" cy="12.0003" r="2.12727" fill="#FABD2E" />
+      <circle cx="5.12727" cy="5.12739" r="2.12727" fill="#FABD2E" />
+      <rect x="5" y="3.00012" width="14" height="4.24999" fill="#FABD2E" />
+      <rect x="5" y="9.87" width="6.99998" height="4.24999" fill="#FABD2E" />
+    </svg>
+
+  ),
+})
