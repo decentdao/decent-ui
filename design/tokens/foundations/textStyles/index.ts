3d319856a5436d1bef061e5b1c1c68d9d902decb
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-04-04 01:49:17 -0400

    add v1 typography parser for new token export

--- design/tokens/foundations/textStyles/index.ts
+++ design/tokens/foundations/textStyles/index.ts
@@ -1,15 +1,17 @@
-import { parseTypography } from '../../../helpers';
-import tokenData from '../../design-tokens-export.json'
+import { parseTypography } from "../../../helpers"
+import { parseTypographyTokens } from "./parseTypography"
+import tokenData from "../../design-tokens-export.json"
 
-const { font: { sans, mono } } = tokenData
+const {
+  font: { sans, mono },
+} = tokenData
 
 export const sansTypography: { [key: string]: { [key: string]: {} } } = parseTypography(sans)
 export const monoTypography: { [key: string]: { [key: string]: {} } } = parseTypography(mono)
 
-// @todo add support for v1 typography
-// const v1Font: { [key: string]: { [key: string]: {} } } = parsedTypographyV1()
-
+const typography = parseTypographyTokens()
 export default {
-    ...sansTypography,
-    ...monoTypography
+  ...typography,
+  ...sansTypography,
+  ...monoTypography,
 }
