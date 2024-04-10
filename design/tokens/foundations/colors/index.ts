4e5bd9fbfa54436f1b6e7c4148facd490d286ff4
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-04-10 00:39:20 -0400

    add new exported tokens and update function to extract tokens

--- design/tokens/foundations/colors/index.ts
+++ design/tokens/foundations/colors/index.ts
@@ -1,24 +1,32 @@
-import { parseColors } from '../../../helpers';
-import tokenData from '../../design-tokens-export.json'
-import { parseColorTokens } from './parseColors';
+import colorTokens from "./color.styles.tokens.json"
 
-const { color } = tokenData;
-const { primary, secondary, neutral, utility, ui } = color;
-const primaries = parseColors({ ...primary })
-const secondaries = parseColors({ ...secondary })
-const neutrals = parseColors({ ...neutral })
-const utilities = parseColors({ ...utility })
-const uiColors = parseColors({ ...ui })
-
-const colorsv1 = parseColorTokens()
-const colors = {
-    ...primaries,
-    ...secondaries,
-    ...neutrals,
-    ...uiColors,
-    ...utilities,
-    ...colorsv1
+interface ColorValue {
+  $value?: string
 }
 
-export type Colors = typeof colors;
-export default colors;
+interface ColorToken {
+  [key: string]: ColorValue | ColorToken
+}
+
+const extractColorsFromJson = (tokens: ColorToken, prefix = ""): Record<string, string> => {
+  const colors: Record<string, string> = {}
+  Object.entries(tokens).forEach(([key, value]) => {
+    // Replace spaces with dashes in the key
+    const formattedKey = key.replace(/\s+/g, "-").toLowerCase()
+    if (typeof value === "object" && value !== null) {
+      if ("$value" in value && typeof value.$value === "string") {
+        colors[`${prefix}${formattedKey}`] = value.$value
+      } else {
+        const nestedColors = extractColorsFromJson(value as ColorToken, `${prefix}${formattedKey}-`)
+        Object.assign(colors, nestedColors)
+      }
+    }
+  })
+
+  return colors
+}
+
+const colors = extractColorsFromJson(colorTokens as unknown as ColorToken)
+
+export type Colors = typeof colors
+export default colors
--- design/tokens/foundations/textStyles/index.ts
+++ design/tokens/foundations/textStyles/index.ts
@@ -1,18 +1,44 @@
-import { parseTypography } from "../../../helpers"
-import { parseTypographyTokens } from "./parseTypography"
-import tokenData from "../../design-tokens-export.json"
+import textStylesTokens from './text.styles.tokens.json';
 
-const {
-  font: { sans, mono },
-} = tokenData
-
-export const sansTypography: { [key: string]: { [key: string]: {} } } = parseTypography(sans)
-export const monoTypography: { [key: string]: { [key: string]: {} } } = parseTypography(mono)
-
-export const typography = parseTypographyTokens()
-
-export default {
-  ...typography,
-  ...sansTypography,
-  ...monoTypography,
+export interface TypographyValue {
+  fontFamily: string;
+  fontSize: string;
+  fontWeight: number | string;
+  letterSpacing: string;
+  lineHeight: string;
+  textTransform: string;
+  textDecoration: string;
 }
+
+interface TypographyToken {
+  $type: string;
+  $value: TypographyValue;
+}
+
+interface TypographyTokenGroup {
+  [key: string]: TypographyToken;
+}
+
+interface TypographyScheme {
+  [groupName: string]: TypographyTokenGroup;
+}
+
+const extractTypographyFromJson = (tokens: TypographyScheme, prefix = '') => {
+  const typography: Record<string, TypographyValue> = {};
+  Object.entries(tokens).forEach(([key, value]) => {
+    Object.entries(value).forEach(([innerKey, innerValue]) => {
+      const formattedInnerKey = innerKey.replace(/\s+/g, '-').toLowerCase();
+      if (innerValue && typeof innerValue === 'object' && innerValue.$value) {
+        typography[`${formattedInnerKey}`] = innerValue.$value;
+      }
+    });
+  });
+
+  return typography;
+};
+
+const typography = extractTypographyFromJson(textStylesTokens as unknown as TypographyScheme);
+console.log(typography);
+
+export type Typography = typeof typography;
+export default typography;
