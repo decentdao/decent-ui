c01b8b60ab9c7214a6578c62f8092ffbbcc5206d
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-04-11 00:17:26 -0400

    update textarea with new design styling

--- design/tokens/components/textarea/theme/textarea.sizes.ts
+++ design/tokens/components/textarea/theme/textarea.sizes.ts
@@ -1,37 +1,22 @@
-import { defineStyle } from '@chakra-ui/react'
+import { defineStyle } from "@chakra-ui/react"
 
-
-const paddingBase = { px: '1rem'}
-const paddingAddonLeft = { pl: '2rem', pr: '1rem'}
-const paddingAddonRight = { pl: '1rem', pr: '4rem'}
+const paddingBase = { px: "1rem" }
+const paddingAddonLeft = { pl: "2rem", pr: "1rem" }
+const paddingAddonRight = { pl: "1rem", pr: "4rem" }
 
 const baseStyle = {
-  apply: 'text-base-mono-semibold',
-  height: '2.5rem',
-  py: '1rem',
-  _placeholder: {
-    textStyle: 'text-base-mono-medium'
-  },
-}
-
-const xlStyle = {
-  apply: 'text-xl-mono-semibold',
-  py: '0.5rem',
-  h: '4.375rem',
-  _placeholder: {
-    textStyle: 'text-xl-mono-medium',
-    lineHeight: '24px',
-  },
+  apply: "input-text",
+  py: "1rem",
 }
 
 const base = defineStyle({
   ...baseStyle,
-  ...paddingBase
+  ...paddingBase,
 })
 
 const baseAddonLeft = defineStyle({
   ...baseStyle,
-  ...paddingAddonLeft
+  ...paddingAddonLeft,
 })
 const baseAddonRight = defineStyle({
   ...baseStyle,
@@ -41,27 +26,7 @@ const baseAddonRight = defineStyle({
 const baseWithAddons = defineStyle({
   ...baseStyle,
   ...paddingAddonRight,
-  ...paddingAddonLeft
-})
-
-const xl = defineStyle({
-  ...xlStyle,
-  ...paddingBase
-})
-
-const xlAddonLeft = defineStyle({
-  ...xlStyle,
-  ...paddingAddonLeft
-})
-const xlAddonRight = defineStyle({
-  ...xlStyle,
-  ...paddingAddonRight
-})
-
-const xlWithAddons = defineStyle({
-  ...xlStyle,
-  ...paddingAddonRight,
-  ...paddingAddonLeft
+  ...paddingAddonLeft,
 })
 
 const sizes = {
@@ -69,10 +34,6 @@ const sizes = {
   baseAddonLeft,
   baseAddonRight,
   baseWithAddons,
-  xl,
-  xlAddonLeft,
-  xlAddonRight,
-  xlWithAddons,
 }
 
 export default sizes
