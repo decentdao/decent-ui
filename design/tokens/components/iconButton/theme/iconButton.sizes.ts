aac38c6e275f01984b099124d8be5919fc59ecdb
Kirill Klimenko <klimenkomud@gmail.com>
2024-05-10 23:57:56 +0200

    Adjust disabled button color for primary button, add styles for iconButton

---
+++ design/tokens/components/iconButton/theme/iconButton.sizes.ts
@@ -0,0 +1,21 @@
+export default {
+  lg: {
+    apply: 'utility-styles-icon-button-large',
+    height: '2rem',
+    width: '2rem',
+    padding: '0.5rem',
+    borderRadius: '0.5rem'
+  },
+  md: {
+    apply: 'utility-styles-icon-button-base',
+    height: '1.5rem',
+    width: '1.5rem',
+    padding: '0.25rem',
+  },
+  sm: {
+    apply: 'utility-styles-icon-button-small',
+    height: '1.25rem',
+    width: '1.25rem',
+    padding: '0.25rem'
+  }
+}
---
+++ design/tokens/components/iconButton/theme/index.ts
@@ -0,0 +1,17 @@
+import { defineStyleConfig } from '@chakra-ui/react';
+
+import baseStyle from './iconButton.base'
+import variants from './iconButton.variants'
+import sizes from './iconButton.sizes'
+
+const IconButton = defineStyleConfig({
+  baseStyle,
+  variants,
+  sizes,
+  defaultProps: {
+    size: 'md',
+    variant: 'primary',
+  },
+});
+
+export default IconButton
