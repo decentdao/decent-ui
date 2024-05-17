6db36a3e50d8e02a8d1b66b6de8a941a54f7ae7e
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-05-17 10:37:58 -0400

    rename and update scroll class

--- design/tokens/styles/scroll.ts
+++ design/tokens/styles/scroll.ts
@@ -1,17 +1,17 @@
 export default {
-  'menu-scroll::-webkit-scrollbar': {
+  'scroll-dark::-webkit-scrollbar': {
     background: 'transparent',
     width: '0.5rem',
     height: '0.5rem',
   },
-  'menu-scroll::-webkit-scrollbar-thumb': {
+  'scroll-dark::-webkit-scrollbar-thumb': {
     border: 'none',
     boxShadow: 'none',
-    background: 'grayscale.500',
+    background: 'neutral-4',
     borderRadius: '0.5rem',
     minHeight: '2.5rem',
   },
-  'menu-scroll::-webkit-scrollbar-thumb:hover': {
-    backgroundColor: 'grayscale.300',
+  'scroll-dark::-webkit-scrollbar-thumb:hover': {
+    backgroundColor: 'neutral-4',
   },
 }
